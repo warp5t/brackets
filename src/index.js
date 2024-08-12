42f7345905b7250b2839d6d27ef3ecf5b9d248c0
warp5t <dmitrij.didencko@yandex.by>
2024-08-12 18:46:09 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ const spreadEl = bracketsConfig.map(item => item.join(''));
+  while (spreadEl.length) {
+    let counter = 0;
+    spreadEl.forEach((el) => {
+      if (str.indexOf(el) != -1) {
+        str = str.replace(el, '');
+      } else {
+        counter += 1;
+      }
+    });
+
+    if (counter === spreadEl.length) {
+      spreadEl.length = 0;
+    }
+  }
+  return str.length === 0 ? true : false;
 }
